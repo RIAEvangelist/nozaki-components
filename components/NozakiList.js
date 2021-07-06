2a778c12c5ef44b1dfea5ea68be694a32833016f
Brandon Nozaki Miller <brandon@diginow.it>
2021-07-06 08:09:30 +0000

    added a list

---
+++ components/NozakiList.js
@@ -0,0 +1,27 @@
+const style=`<link rel="stylesheet" href="../css/w3.css">`;
+
+function getTemplate(self){
+    return `
+        ${style}
+        <ul class="w3-ul w3-hoverable ${self.dataset.class||''}"
+            style="${self.dataset.style||''}"
+        >
+            ${self.innerHTML}
+        </ul>
+    `;
+}
+
+class NozakiList extends HTMLElement {
+    constructor() {
+        super();
+        
+        //this.shadowRoot
+        this.attachShadow({mode: 'open'});
+        
+        this.shadowRoot.innerHTML=getTemplate(this);
+
+        return this;
+    }
+}
+
+customElements.define('nozaki-list', NozakiList);
