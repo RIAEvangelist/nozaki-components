987b17030810037ad37ca420dd09f3d4c68f64f3
Brandon <brandon@diginow.it>
2021-07-21 14:26:58 -0700

    https

--- public.js
+++ public.js
@@ -1,13 +1,39 @@
 import express from 'express';
+import http from 'http';
+import https from 'https';
+import fs from 'fs';
+
 const app = express();
-const port = 80;
+
+// Certificate
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/nozaki.ninja/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/nozaki.ninja/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/nozaki.ninja/chain.pem', 'utf8');
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
+
 
 // Setting up the public directory
-app.use(express.static('./'));
+app.use(express.static('./',{ dotfiles: 'allow' }));
 
-app.listen(
-    port, 
-    () => console.log(
-        `http://localhost:${port}!`
-    )
+// Starting both http & https servers
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(
+    80, 
+    ()=>{
+    	console.log('HTTP Server running on port 80');
+    }
+);
+
+httpsServer.listen(
+    443, 
+    ()=>{
+    	console.log('HTTPS Server running on port 443');
+    }
 );
